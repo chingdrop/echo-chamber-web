e37e53cbbb96f35a9717ab774cdb620ceacf02f8
Craig Hurley <chingdorp@pm.me>
2025-03-10 09:53:26 -0400

    Create the Result model

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Result(models.Model):
+    crawl_name = models.CharField(max_length=125)
+    target_link = models.URLField(max_length=255)
+    results_json = models.JSONField()
+
+    def __str__(self):
+        return self.crawl_name
