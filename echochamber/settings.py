35c6492882b0afb1a1f4062b647b426425b2a8d9
Craig Hurley <chingdorp@pm.me>
2025-03-10 14:32:31 -0400

    store django secret in .env file

--- echochamber/settings.py
+++ echochamber/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 # --- Leave these unused imports here, I don't know if they're being used!
+import os
 import logging
-from pathlib import Path
 import colorlog
+from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1u5clpkwc--c2=e-)e^701y3u&u((qfmz+lbrzzm(o#i(27v2)'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY_FALLBACKS = ["django-insecure-1u5clpkwc--c2=e-)e^701y3u&u((qfmz+lbrzzm(o#i(27v2)"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
