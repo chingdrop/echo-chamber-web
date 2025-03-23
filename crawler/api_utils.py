5846e2c5c0f3d85d891daa58a5294eb5276eb1a2
Craig Hurley <chingdorp@pm.me>
2025-03-23 11:58:57 -0400

    Google search api interaction

--- crawler/api_utils.py
+++ crawler/api_utils.py
@@ -1,5 +1,11 @@
 import requests
 import logging
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from time import sleep
+from urllib.parse import unquote
+
+from echo_scraper.items import GoogleResultItem
 
 
 class RestAdapter:
@@ -214,3 +220,78 @@ class RestAdapter:
                                   verify=verify,
                                   timeout=timeout,
                                   allow_redirects=allow_redirects)
+    
+
+class GoogleSearch:
+    def __init__(self, proxy=None, logger=None):
+        ua = UserAgent()
+        headers = {
+            'User-Agent': ua.chrome,
+            'Accept': "*/*",
+        }
+        proxies = {"https": proxy, "http": proxy} \
+            if proxy and (proxy.startswith("https") or proxy.startswith("http") or proxy.startswith("socks5")) \
+            else None
+        self.rest = RestAdapter(headers=headers, proxies=proxies, logger=logger)
+        self.logger = logger or logging.getLogger(__name__)
+        
+    def _request(self, term: str, results: int, lang: str, start: int, safe: str, region: str):
+        params = {
+            'q': term,
+            'num': results + 2,
+            'hl': lang,
+            'start': start,
+            'safe': safe,
+            'gl': region
+        }
+        cookies = {
+            'CONSENT': 'PENDING+987',
+            'SOCS': 'CAESHAgBEhIaAB',
+        }
+        return self.rest.get('https://www.google.com/search',
+                                 params=params,
+                                 cookies=cookies,
+                                 timeout=5)
+    
+    def _parse_results(self, response_text):
+        soup = BeautifulSoup(response_text, 'html.parser')
+        result_block = soup.find_all("div", class_="ezO2md")
+        for result in result_block:
+            link_tag = result.find("a", href=True)
+            title_tag = link_tag.find("span", class_="CVA68e") if link_tag else None
+            description_tag = result.find("span", class_="FrIlee")
+
+            if link_tag and title_tag and description_tag:
+                link = unquote(link_tag["href"].split("&")[0].replace("/url?q=", ""))
+                title = title_tag.text if title_tag else ""
+                description = description_tag.text if description_tag else ""
+                yield GoogleResultItem(link, title, description)
+
+    def search(self, term, results=10, lang='en', start=0, safe='active', region=None, unique=False):
+        fetched_results = 0
+        fetched_links = set()
+
+        while fetched_results < results:
+            response_text = self._request(term, results, lang, start, safe, region)
+            if not response_text:
+                break  # Stop the search if the request fails
+
+            new_results = 0
+            for search_result in self._parse_results(response_text):
+                if search_result.link in fetched_links and unique:
+                    continue  # Skip this result if the link is not unique
+
+                fetched_links.add(search_result.link)
+                fetched_results += 1
+                new_results += 1
+                yield search_result
+
+                if fetched_results >= results:
+                    return  # Stop if we have fetched the desired number of results
+
+            if new_results == 0:
+                logging.info(f"Only {fetched_results} results found for query requiring {results} results.")
+                break  # Break the loop if no new results were found in this iteration
+
+            start += 10
+            sleep(120)  # Avoid hitting Google's rate limits
