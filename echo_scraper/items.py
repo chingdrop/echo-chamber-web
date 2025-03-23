7e6d484923a8d0189e9a0ff4538af09506cec619
Craig Hurley <chingdorp@pm.me>
2025-03-23 11:59:30 -0400

    Save items as models in django

--- echo_scraper/items.py
+++ echo_scraper/items.py
@@ -3,10 +3,21 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
+
+from crawler.models import CrawlResult
 
 
-class EchoScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class GoogleResultItem(Item):
+    link = Field()
+    title = Field()
+    description = Field()
+
+    def save(self):
+        google_result = GoogleResult(
+            link=self['link'],
+            title=self['title'],
+            description=self['description']
+        )
+        google_result.save()
+        return google_result
