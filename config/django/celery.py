96eaef7a50a866e08a1fd189645b84d68359aa90
Craig Hurley <chingdorp@pm.me>
2025-03-26 18:44:14 -0400

    fixed django settings paths

--- config/django/celery.py
+++ config/django/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "echo_chamber.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.settings")
 
 app = Celery("echo_chamber")
 
